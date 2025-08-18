3fed113b5cb42f2074cd61de3d33d0d14c2f60b0
igloria23 <igloriachristian159@gmail.com>
2025-08-18 22:18:05 +0800

    Activity9.py

---
+++ Activity9.py
@@ -0,0 +1,8 @@
+username = 'christian'
+password = 'pogiako405'
+
+print(username == 'christian')  # True
+print(password == 'pogiako405')  # True
+print((username == 'christian') or (password == 'pogiako405'))  # True
+print((username == 'christian') and (password == 'pogiako405'))
+print(not((username == 'christian') and (password == 'pogiako405')))
