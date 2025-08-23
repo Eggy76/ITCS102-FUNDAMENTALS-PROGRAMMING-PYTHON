517573c4d63834da381228a87178458cb1501aa1
igloria23 <igloriachristian159@gmail.com>
2025-08-23 10:00:17 +0800

    jeepfare_fixed

--- Activity10.py
+++ Activity10.py
@@ -4,12 +4,13 @@
 #discount
 
 passenger = input("Input your name: ")
-passenger1 = input("Are you a student (Yes / No): ")
+passenger1 = input("Are you a student (yes / no): ")
 fare = eval(input("Pay: "))
 
-if passenger == "yes" :
-	discount = fare * .15
-	new_fare = fare - .15
+if passenger1.lower() == "yes" :
+	discount = fare * .16
+	new_fare = fare - 2.0
 	print("Congrats discount has been approved your newfare is",new_fare,  "Enjoy your ride with us")
 else:
 	print("Sorry your request on the discount has not been approved")
+	
