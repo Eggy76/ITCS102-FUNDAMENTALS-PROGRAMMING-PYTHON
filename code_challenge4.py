5a788b854c3689b3ae35c841ae4e174745fc9136
igloria23 <igloriachristian159@gmail.com>
2025-08-27 11:25:21 +0800

    Add files via upload
    odd or even kineme

---
+++ code_challenge4.py
@@ -0,0 +1,10 @@
+#input number
+#identify odd or even
+#key % and ==
+
+number = int(input("Input a number: "))
+if number % 2 == 0:
+
+	print("The number is even")
+else:
+	print("The number is Odd")
