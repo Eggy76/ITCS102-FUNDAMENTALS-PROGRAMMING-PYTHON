a2199769472b98acd66fc0804e9b82b421ec0a0d
igloria23 <igloriachristian159@gmail.com>
2025-10-12 07:41:48 +0800

    code_challenge13.py
    christmas_tree

--- code_challenge13.py
+++ code_challenge13.py
@@ -1,10 +1,58 @@
 
 
-for i in range(1,7,1):
-    for x in range(7,i,-1):
+for i in range(1,3,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
         print(" ",end=" ")
-    for y in range(i,0,-1):
-        print(y,end=" ")
-    for y in range(2,x,1):
-        print(y,end=" ")
+    print()
+for i in range(3,0,-1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ")
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,7,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,13,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,15,1):
+    for i in range(15,i,-1):
+        print(" ",end=" ") 
+    for x in range(0,i,1):
+        print("*",end=" ") 
+    for x in range(1,i,1):
+        print("*",end=" ")
+    for i in range(10,i,-1):
+        print(" ",end=" ")
+    print()
+for i in range(1,11,1):
+    for i in range(11,1,-1):
+        print(" ",end=" ") 
+    for x in range(0,9,1):
+        print("*",end=" ") 
     print()
