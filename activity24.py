eb32c625f997df98b2ba9ab0d8e77ff2a27062f4
igloria23 <igloriachristian159@gmail.com>
2025-11-07 11:07:03 +0800

    summation
    summation_and_loop

---
+++ activity24.py
@@ -0,0 +1,16 @@
+
+
+def greeter(name):
+    print(f"Hi, {name} my lalabs ")
+
+def summation(a):
+    sum = 0
+    for b in range(a, 0, -1):
+        sum += b
+    print(f'So The sum of {a} is {sum}')
+
+summation(1)
+summation(2)
+summation(11)
+summation(12)
+summation(15)
