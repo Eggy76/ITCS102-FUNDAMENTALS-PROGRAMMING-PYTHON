70a1cbe7588487c4c9368c14e2d28eb85694bb63
igloria23 <igloriachristian159@gmail.com>
2025-11-29 08:18:05 +0800

    Add activities for personal introduction and input

---
+++ activity25_1.py
@@ -0,0 +1,21 @@
+def activity1():
+    print("Hi, I am Christian L. Igloria, student from the BSIT First Year")
+    print("Section 1A")
+
+    print("This is my first file in my REPO.")
+
+    print("Pogi po Sir")
+
+    print("About Me. :")
+    print("From Brgy Ibabang Dupay Lucena City, I love singing and playing guitar.")
+
+def activity2():
+    name = input("Input your name: ")
+    print("Hi ",name," Welcome to the matrix.")
+def activity3():
+    print("Happy \t Monday")
+    print("BSIT 1A from DLL")
+def activity4():
+    name = input("Enter a string -> ")
+
+    print("Your name has ", len(name), "character")
